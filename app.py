92ceb20dea168b5bf015a59c122a9e04c89c200f
Eray Kırca <81390850+eraykirca@users.noreply.github.com>
2025-09-28 07:33:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 # Title
-st.title("Hello Streamlit!")
+st.title("tttttttttttttttttttttttttttt")
 
 # Input from user
 name = st.text_input("Enter your name:")
 
 # Button action
 if st.button("Say Hello"):
+
     st.write(f"Hello, {name} 👋 Welcome to Streamlit!")
